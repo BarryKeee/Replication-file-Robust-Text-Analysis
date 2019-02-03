f151aab576a1aa51470aa66ab9c3586328f1cb91
BarryKeee <35748027+BarryKeee@users.noreply.github.com>
2019-02-02 19:23:49 -0500

    Create generate_term_document.py

---
+++ generate_term_document.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 21:18:59 2019
+
+@author: barry
+"""
+
+
+import pandas as pd
+from gensim import corpora
+
+
+def generate_term_document():
+    
+    data = pd.read_excel('raw_token.xlsx')
+        
+    document_content = []
+    for statement in data['Content'].values:
+        try:
+            document_content.append(statement.split(' '))
+        except AttributeError:
+            document_content.append([])
+            
+    dictionary = corpora.Dictionary(document_content)
+    corpus = [dictionary.doc2bow(text) for text in document_content]
+    #print(dictionary.token2id)
+    
+    
+    term_document = pd.DataFrame(index = dictionary.token2id.values())
+    for i in range(len(corpus)):
+        try:
+            dist = pd.DataFrame(corpus[i]).set_index(0)
+            term_document[i] = dist[1]
+        except KeyError:
+            term_document[i] = 0
+        
+        
+    term_document = term_document.fillna(0)
+    
+    return term_document
