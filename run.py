c5222060b9bb25b41212c4b1113826f45a8fbadb
BarryKeee <35748027+BarryKeee@users.noreply.github.com>
2019-02-06 09:52:45 -0500

    comment

--- run.py
+++ run.py
@@ -5,9 +5,15 @@ Created on Tue Feb 05 21:57:54 2019
 @author: barry
 """
 
+
+''' function to generate raw document files from pdfs'''
 from generate_raw_data import generate_raw_data
+''' function to process raw documents '''
 from preprocess import preprocess
+''' function to generate term-document matrix '''
 from generate_term_document import generate_term_document
+
+
 
 raw_document = generate_raw_data()
 tokenized_document = preprocess()
